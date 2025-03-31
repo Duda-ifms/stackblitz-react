6e3f53425a76f19a3d6e8d5479afab2f6166d035
Duda-ifms <et49181@gmail.com>
2025-03-31 19:53:28 -0300

    31/03

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyALZpVbt65LIq0aG_nfH2tYfOhVuSeIpU',
-  authDomain: 'react-login-128c3.firebaseapp.com',
-  projectId: 'react-login-128c3',
-  storageBucket: 'react-login-128c3.appspot.com',
-  messagingSenderId: '50027308831',
-  appId: '1:50027308831:web:81dc51b74c07c7a57fce2a',
-  measurementId: 'G-LG701FCDCR'
+  apiKey: "AIzaSyAh5XXgHqtddD1rqmMOPN5twk9UDCWRKVA",
+  authDomain: "ptac-projeto.firebaseapp.com",
+  projectId: "ptac-projeto",
+  storageBucket: "ptac-projeto.firebasestorage.app",
+  messagingSenderId: "245792746993",
+  appId: "1:245792746993:web:041cc4ac64cd1c923ae850",
+  measurementId: "G-CV0VB3L77C"
 };
 
 const app = initializeApp(firebaseConfig);
